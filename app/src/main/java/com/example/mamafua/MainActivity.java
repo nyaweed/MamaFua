0a1878cb8a3099ab5bcf8f2f01559e9723a9af31
nyaweed <nyamsbrian@gmail.com>
2023-03-15 16:33:55 +0300

    stranded

--- app/src/main/java/com/example/mamafua/MainActivity.java
+++ app/src/main/java/com/example/mamafua/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.mamafua;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
     }
 }
