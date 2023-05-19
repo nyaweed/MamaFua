583e47e49d86a2fc1aa9af4c3b1046df12d4f45b
nyaweed <nyamsbrian@gmail.com>
2023-05-19 20:21:05 +0300

    bottom navigation bar modified

--- app/src/main/java/com/example/mamafua/MainActivity.java
+++ app/src/main/java/com/example/mamafua/MainActivity.java
@@ -1,17 +1,19 @@
 package com.example.mamafua;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    ActionBar actionBar;
+    BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +21,60 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         FirebaseApp.initializeApp(this);
         //onclick listener
-        ImageButton btn = findViewById(R.id.accButton);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, LogoutActivity.class);
-                startActivity(intent);
-            }
-        });
+
+        actionBar = getSupportActionBar();
+
+        navigationView = findViewById(R.id.navigation);
+        navigationView.setOnNavigationItemSelectedListener(selectedListener);
+        actionBar.setTitle("Home");
+
+        HomeFragment fragment = new HomeFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.content, fragment, "");
+        fragmentTransaction.commit();
 
     }
+    //switch case scenario
+
+    private BottomNavigationView.OnNavigationItemSelectedListener selectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+
+                case R.id.home_item:
+                    actionBar.setTitle("Home");
+                    HomeFragment fragment = new HomeFragment();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.content, fragment, "");
+                    fragmentTransaction.commit();
+                    return true;
+
+                case R.id.search_item:
+                    actionBar.setTitle("Profile");
+                    SearchFragment fragment1 = new SearchFragment();
+                    FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction1.replace(R.id.content, fragment1);
+                    fragmentTransaction1.commit();
+                    return true;
+
+                case R.id.notification_item:
+                    actionBar.setTitle("Users");
+                    NotifyFragment fragment2 = new NotifyFragment();
+                    FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction2.replace(R.id.content, fragment2, "");
+                    fragmentTransaction2.commit();
+                    return true;
+
+                case R.id.messages_item:
+                    actionBar.setTitle("Chats");
+                    MessagesFragment listFragment = new MessagesFragment();
+                    FragmentTransaction fragmentTransaction3 = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction3.replace(R.id.content, listFragment, "");
+                    fragmentTransaction3.commit();
+                    return true;
+
+            }
+            return false;
+        }
+    };
 }
